b3098a7ec298909bb2f784a3c3223030b40971e2
Pham Long Duy Tien <duytien09092002@gmail.com>
2023-10-11 11:47:43 +0700

    Basic UI
    Many functions of the app has not been implemented yet, except for the Statistics button that leads to a new screen.

---
+++ app/src/main/java/com/tdtu/mywallet/MainMenu.java
@@ -0,0 +1,24 @@
+package com.tdtu.mywallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+
+    public void onClickStatistics(View view) {
+
+        Intent intent = new Intent(MainMenu.this, StatisticsMenu.class);
+        startActivity(intent);
+
+    }
+}
